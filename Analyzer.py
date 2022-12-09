2843a4cac5a171217a82c2a95e1f638f2090e026
Patrick Vallely <83255314+AstronomerPat@users.noreply.github.com>
2022-12-09 15:13:13 -0800

    Add files via upload
    Final data set, plus minor tweaks to analysis script to avoid reliance on "today"

--- Analyzer.py
+++ Analyzer.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import date, timedelta
 from matplotlib.ticker import MaxNLocator
+from scipy.optimize import curve_fit
 
 # import the LinearRegression object
 from sklearn.linear_model import LinearRegression
@@ -9,52 +10,75 @@ from sklearn.linear_model import LinearRegression
 #plt.rc('text',usetex=True)
 plt.rc('font',family='serif')
 
-InitWeight = 250.0 #Weight on 12/31/2022, in lbs
+InitDate = date(2021,12,31)
+
+InitWeight = 250.0 #Weight on 12/31/2021, in lbs
 GoalWeight = 200.0 #Desired goal weight
 
 BasecCals = 2350.0 #Assumed base metabolic rate (the "2000 Calories" on the back of every snack)
-GoalCals = 1350.0 #Goal for net calories per day
+GoalCals = 1500.0 #Goal for net calories per day
 
-Dates = []
-DateInts = []
+DateIntsW = []
 Weights = []
+DateIntsC = []
 FoodCals = []
 ExerciseCals = []
 
 with open("CaloriesData.txt") as f:
     lines = f.readlines()
-    for line in lines:
-        if (not "WWWWW" in line) and (not "Food" in line): #skip header and unfilled entries
-            Weight = float( line.split()[2].replace('*','') )
-            Date = line.split()[0]
-            DateInt = int( line.split('    ')[1] )
-            if "CCCC" in line or "EEE" in line:
-                #assume that this incomplete data entry will match the prior averages
-                FoodCal = np.median(np.array( FoodCals ))
-                ExerciseCal = np.median(np.array( ExerciseCals ))
-            else:
+    for line in lines[1:]:
+        if not ("WWWWW" in line and "CCCC" in line): #skip completely unfilled entries
+            if "WWWWW" in line:
+                DateInt = int( line.split('    ')[1] )
                 FoodCal = float( line.split()[3] )
                 ExerciseCal = float( line.split()[4].replace('\n','') )
-            Dates.append(Date)
-            DateInts.append(DateInt)
-            Weights.append(Weight)
-            FoodCals.append(FoodCal)
-            ExerciseCals.append(ExerciseCal)
+                DateIntsC.append(DateInt)
+                FoodCals.append(FoodCal)
+                ExerciseCals.append(ExerciseCal)
+            elif "CCCC" in line:
+                #assume that this incomplete data entry will match the prior medians
+                Weight = float( line.split()[2])
+                #FoodCal = np.median(np.array( FoodCals ))
+                #ExerciseCal = np.median(np.array( ExerciseCals ))
+                DateInt = int( line.split('    ')[1] )
+                DateIntsW.append(DateInt)
+                Weights.append(Weight)
+                #DateIntsC.append(DateInt)
+                #FoodCals.append(FoodCal)
+                #ExerciseCals.append(ExerciseCal)
+            else:
+                #normal days with full weight and calorie data
+                Weight = float( line.split()[2])
+                DateInt = int( line.split('    ')[1] )
+                FoodCal = float( line.split()[3] )
+                ExerciseCal = float( line.split()[4].replace('\n','') )
+                DateIntsW.append(DateInt)
+                Weights.append(Weight)
+                DateIntsC.append(DateInt)
+                FoodCals.append(FoodCal)
+                ExerciseCals.append(ExerciseCal)
 
 # make data into numpy arrays for convenience
-Dates = np.array(Dates)
-DateInts = np.array(DateInts)
+DateIntsW = np.array(DateIntsW)
+DateIntsC = np.array(DateIntsC)
 Weights = np.array(Weights)
 FoodCals = np.array(FoodCals)
 ExerciseCals = np.array(ExerciseCals)
 # get net calories
 NetCals = FoodCals - ExerciseCals
 
+CurrentDate = InitDate + timedelta(days=float(max(DateIntsC)))
+CurrentMonth = CurrentDate.strftime("%B")
+CurrentDay = str(CurrentDate.day)
+CurrentYear = str(CurrentDate.year)
+
 #CurrentWeight = Weights[-1]
 #CurrentWeight = np.mean(Weights[-5:])
 CurrentWeight = min(Weights[-7:])
 print()
-print("---------- Analyzing "+str(len(DateInts))+" Days of Data ----------")
+print("---------- Analyzing "+str(len(DateIntsC))+" Days of Data ----------")
+print("Current Date: "+CurrentMonth+" "+CurrentDay+", "+CurrentYear)
+print()
 print("Starting Weight: "+str(InitWeight)+" lbs")
 print("Current Weight:  "+str(round(CurrentWeight,1))+" lbs")
 print("Goal Weight:     "+str(GoalWeight)+" lbs")
@@ -113,21 +137,25 @@ for i in range(len(NetCals)):
 
 plt.figure(figsize=(10,8.0))
 
+
 plt.subplot(2, 2, 1)
 
+cscale = 3
+bkgscale = 8
 
-plt.bar(DateInts, NetCals+ExerciseCals, edgecolor='0.75', color='1.0', hatch='/////', label="Exercise")
+#plt.bar(DateIntsC, NetCals+ExerciseCals, edgecolor='0.75', color='1.0', hatch='/////', label="Exercise", width=1.0)
+plt.bar(DateIntsC, NetCals+ExerciseCals, color='0.75', label="Exercise", width=1.0)
 for i in range(len(NetCals)):
     if NetCals[i] <= GoalCals:
-        plt.bar(DateInts[i], NetCals[i], color='limegreen')
+        plt.bar(DateIntsC[i], NetCals[i], color='limegreen', width=1.0,alpha=0.7)
     else:
-        plt.bar(DateInts[i], NetCals[i], color='red')
+        plt.bar(DateIntsC[i], NetCals[i], color='red', width=1.0,alpha=0.7)
     if NetCals_RollingAvg[i] <= GoalCals:
-        plt.scatter(DateInts[i], NetCals_RollingAvg[i], color='limegreen', s=7, zorder=50000)
+        plt.scatter(DateIntsC[i], NetCals_RollingAvg[i], color='limegreen', s=cscale, zorder=50000)
     else:
-        plt.scatter(DateInts[i], NetCals_RollingAvg[i], color='red', s=7, zorder=50000)
-plt.scatter(DateInts, NetCals_RollingAvg, color='black', s=18, zorder=50000-1)
-plt.plot(DateInts, NetCals_RollingAvg, color='black', linestyle='--', linewidth=1.0, zorder=50000-1, label="7-Day Rolling Avg.")
+        plt.scatter(DateIntsC[i], NetCals_RollingAvg[i], color='red', s=cscale, zorder=50000)
+plt.scatter(DateIntsC, NetCals_RollingAvg, color='black', s=bkgscale, zorder=50000-1)
+plt.plot(DateIntsC, NetCals_RollingAvg, color='black', linestyle='--', linewidth=1.0, zorder=50000-1, label="7-Day Rolling Avg.")
 #if np.mean(NetCals[-7:]) <= GoalCals:
 #    plt.axhline(np.mean(NetCals[-7:]), linestyle=':', color='limegreen', linewidth=1.0, label="Average Calories (Past Week)")
 #else:
@@ -141,40 +169,41 @@ plt.xticks([],fontsize=10)
 plt.yticks(fontsize=10)
 plt.ylabel("Daily Net Calories", fontsize=14)
 plt.ylim(0,4200)
-plt.xlim(min(DateInts)-1,max(DateInts)+0.99)
+plt.xlim(min(DateIntsC)-1,max(DateIntsC)+0.99)
 plt.legend(fontsize=10, loc='upper left', frameon=False)
 
 
 
 plt.subplot(2, 2, 3)
 
-x_fit = np.linspace(min(DateInts)-1,max(DateInts)+1)
+x_fit = np.linspace(min(DateIntsC)-1,max(DateIntsC)+1)
 plt.plot(x_fit, (BasecCals-GoalCals)*x_fit, linestyle='-', color='k', linewidth=1.0,label="Daily Calorie Goal", zorder=50000+1)
 
-plt.bar(DateInts, CumulativeNetCals, color='0.5')
+plt.bar(DateIntsC, CumulativeNetCals, color='0.75', width=1.0)
 
 for i in range(len(NetCals)):
     if CumulativeNetCals[i] >= (i+1)*(BasecCals-GoalCals):
-        plt.scatter(DateInts[i], CumulativeNetCals[i], color='limegreen', s=7, zorder=50000)
+        plt.scatter(DateIntsC[i], CumulativeNetCals[i], color='limegreen', s=cscale, zorder=50000)
     else:
-        plt.scatter(DateInts[i], CumulativeNetCals[i], color='red', s=7, zorder=50000)
-plt.scatter(DateInts, CumulativeNetCals, color='0.5', s=18, zorder=50000-1)
+        plt.scatter(DateIntsC[i], CumulativeNetCals[i], color='red', s=cscale, zorder=50000)
+plt.scatter(DateIntsC, CumulativeNetCals, color='k', s=bkgscale, zorder=50000-1)
 
 plt.xticks(fontsize=10)
 plt.yticks(fontsize=10)
 plt.xlabel("                                                                     Days Into 2022", fontsize=14)
 plt.ylabel("Est. Total Calorie Deficit", fontsize=14)
-plt.xlim(min(DateInts)-1,max(DateInts)+0.99)
+plt.xlim(min(DateIntsC)-1,max(DateIntsC)+0.99)
 plt.legend(fontsize=10, loc='upper left', frameon=False)
 
 
 
-loss_rate = (InitWeight-CurrentWeight) / max(DateInts)
+loss_rate = (InitWeight-CurrentWeight) / max(DateIntsC)
 print()
 print("So far you have been losing about "+str(round(loss_rate,2))+" pounds per day ("+str(round(7.0*loss_rate,2))+" pounds per week).")
 
 GoalDays = ( CurrentWeight - GoalWeight ) / loss_rate
-GoalDateObj = date.today() + timedelta(days=GoalDays)
+#GoalDateObj = date.today() + timedelta(days=GoalDays)
+GoalDateObj = InitDate + timedelta(days=float(max(DateIntsC))) + timedelta(days=GoalDays)
 GoalMonth = GoalDateObj.strftime("%B")
 GoalDay = str(GoalDateObj.day)
 print("If this trend holds, you will reach your goal weight in "+str(int(GoalDays))+" days (on "+GoalMonth+" "+GoalDay+").")
@@ -183,37 +212,69 @@ print("If this trend holds, you will reach your goal weight in "+str(int(GoalDay
 # let's fit a simple linear regression
 slr = LinearRegression(copy_X=True)
 #add the initial point data point from Dec. 31, 2021 manually
-slr.fit(np.insert(DateInts, 0, 0).reshape(-1,1), np.insert(Weights, 0, InitWeight))
+slr.fit(np.insert(DateIntsW, 0, 0).reshape(-1,1), np.insert(Weights, 0, InitWeight))
 
-dateints_fit = np.linspace(-0.5,max(DateInts)+0.5)
+dateints_fit = np.linspace(-0.5,max(DateIntsW)+0.5)
 weights_fit = slr.intercept_ + dateints_fit*slr.coef_[0]
-GoalDays = ( GoalWeight-slr.intercept_ ) / slr.coef_[0] - max(DateInts)
+#GoalDays = ( GoalWeight-slr.intercept_ ) / slr.coef_[0] - max(DateIntsW)
 
-residuals = np.insert(Weights, 0, InitWeight) - slr.predict(np.insert(DateInts, 0, 0).reshape(-1,1))
+#residuals = np.insert(Weights, 0, InitWeight) - slr.predict(np.insert(DateIntsW, 0, 0).reshape(-1,1))
+#residuals_std = np.std( residuals )
+
+
+def multi_component_line(x, m1, b1, t1, m2, t2, m3):
+    y_out = []
+    for x_in in x:
+        if x_in < t1:
+            y = m1*x_in + b1
+        elif x_in >= t1 and x_in < t2:
+            y = m2*(x_in-t1) + m1*t1 + b1
+        elif x_in >= t2:
+            y = m3*(x_in-t2) + m2*(t2-t1) + m1*t1 + b1
+        y_out.append(y)
+    return y_out
+
+mb  = [-0.5 , -0.00001]
+bb  = [50.0 , 500.0]
+t1b = [20.0 ,  60.0]
+t2b = [85.0 , 155.0]
+param_bounds = ((mb[0],bb[0],t1b[0],mb[0],t2b[0],mb[0]),(mb[1],bb[1],t1b[1],mb[1],t2b[1],mb[1]))
+init_guess = [-0.2,250.0,35.0,-0.15,100.0,-0.1]
+
+popt, pcov = curve_fit(multi_component_line, DateIntsW, Weights, p0=init_guess, bounds=param_bounds)
+
+m1_fit = popt[0]
+b1_fit = popt[1]
+t1_fit = popt[2]
+m2_fit = popt[3]
+t2_fit = popt[4]
+m3_fit = popt[5]
+
+
+multi_component_fit = multi_component_line(DateIntsW, m1_fit, b1_fit, t1_fit, m2_fit, t2_fit, m3_fit)
+
+residuals = np.insert(Weights, 0, InitWeight) - multi_component_line(np.insert(DateIntsW, 0, 0), m1_fit, b1_fit, t1_fit, m2_fit, t2_fit, m3_fit)
 residuals_std = np.std( residuals )
 
-'''
-#actually let's force the intercept to be InitWeight
-slr = LinearRegression(copy_X=True, fit_intercept=False)
-#add the initial point data point from Dec. 31, 2021 manually
-slr.fit(np.insert(DateInts, 0, 0).reshape(-1,1), np.insert(Weights, 0, InitWeight)-InitWeight)
-dateints_fit = np.linspace(-0.5,max(DateInts)+0.5)
-weights_fit = InitWeight + dateints_fit*slr.coef_[0]
-GoalDays = ( GoalWeight-InitWeight ) / slr.coef_[0] - max(DateInts)
+GoalDays = (GoalWeight-m2_fit*(t2_fit-t1_fit)-m1_fit*t1_fit-b1_fit)/m3_fit + t2_fit - max(DateIntsW)
 
-residuals = np.insert(Weights, 0, InitWeight) - slr.predict(np.insert(DateInts, 0, 0).reshape(-1,1)) - InitWeight
-residuals_std = np.std( residuals )
-'''
-
-#print()
-#print("So far you have been losing about "+str(round(-slr.coef_[0],2))+" pounds per day ("+str(round(-slr.coef_[0]*7,2))+" pounds per week).")
-
-GoalDateObj = date.today() + timedelta(days=GoalDays)
+#GoalDateObj = date.today() + timedelta(days=GoalDays)
+GoalDateObj = InitDate + timedelta(days=float(max(DateIntsC))) + timedelta(days=GoalDays)
 GoalMonth = GoalDateObj.strftime("%B")
 GoalDay = str(GoalDateObj.day)
+GoalYear = str(GoalDateObj.year)
 
 print()
-print("Based instead on an extrapolation of the plotted linear fit,"+"\n"+"you will reach your goal weight in "+str(int(GoalDays))+" days (on "+GoalMonth+" "+GoalDay+").")
+print("Multi-Component Fit:")
+print("From day 0 to day "+str(int(t1_fit))+", the loss rate is "+str(round(-m1_fit,3))+" lbs/day ("+str(round(-7*m1_fit,2))+" lbs/week)")
+print("From day "+str(int(t1_fit))+" to day "+str(int(t2_fit))+", the loss rate is "+str(round(-m2_fit,3))+" lbs/day ("+str(round(-7*m2_fit,2))+" lbs/week)")
+print("From day "+str(int(t2_fit))+" onward, the loss rate is "+str(round(-m3_fit,3))+" lbs/day ("+str(round(-7*m3_fit,2))+" lbs/week)")
+
+print()
+if GoalDays + max(DateIntsW) > 365.0:
+    print("Based instead on an extrapolation of the multi-component fit described above,"+"\n"+"you will reach your goal weight in "+str(int(GoalDays))+" days (on "+GoalMonth+" "+GoalDay+", "+GoalYear+").")
+else:
+    print("Based instead on an extrapolation of the multi-component fit described above,"+"\n"+"you will reach your goal weight in "+str(int(GoalDays))+" days (on "+GoalMonth+" "+GoalDay+").")
 
 print()
 print("You're doing great! Keep up the good work! :)")
@@ -222,15 +283,19 @@ print()
 
 plt.subplot(2, 2, 2)
 
-#plt.scatter(DateInts, Weights, color='k', marker='o', s=15)
+wscale = 8
+
+
+#plt.scatter(DateIntsW, Weights, color='k', marker='o', s=15)
 #add the initial point data point from Dec. 31, 2021 manually
-plt.scatter(np.insert(DateInts, 0, 0), np.insert(Weights, 0, InitWeight), color='k', marker='o', s=15)
-plt.plot(dateints_fit, weights_fit, color='k', linestyle='-.', linewidth=1.5, label="Linear Fit")
+plt.scatter(np.insert(DateIntsW, 0, 0), np.insert(Weights, 0, InitWeight), color='k', marker='o', s=wscale)
+plt.plot(dateints_fit, weights_fit, color='k', linestyle=':', linewidth=1.5, label="Linear Fit")
+plt.plot(DateIntsW, multi_component_fit, color='k', linestyle='-.', linewidth=1.5, label="Multi-Component Fit")
 #plt.axhline(GoalWeight, linestyle='-', color='gold', linewidth=1.5, label="Goal Weight")
 plt.xticks([],fontsize=10)
 plt.yticks(fontsize=10)
 plt.ylabel("Weight (lbs)", fontsize=14)
-plt.xlim(min(DateInts)-1.5,max(DateInts)+0.5)
+plt.xlim(min(DateIntsW)-2.5,max(DateIntsW)+2.5)
 plt.gca().yaxis.set_ticks_position("right")
 plt.gca().yaxis.set_label_position("right")
 plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
@@ -239,18 +304,19 @@ plt.legend(fontsize=10, loc='upper right', frameon=False)
 
 plt.subplot(2, 2, 4)
 
-plt.text(max(DateInts), min(residuals), "Scatter $\sigma$ = "+str(round(residuals_std,2))+" lbs",
-        ha='right', va='center', fontsize=10)
+plt.text(min(DateIntsW), max(residuals),
+        "Scatter $\sigma$ = "+str(round(residuals_std,2))+" lbs",
+        ha='left', va='center', fontsize=10)
 
-plt.scatter(np.insert(DateInts, 0, 0), residuals, color='k', marker='o', s=15)
+plt.scatter(np.insert(DateIntsW, 0, 0), residuals, color='k', marker='o', s=wscale)
 plt.axhline(0, color='k', linestyle='-.', linewidth=1.5)
 plt.xticks(fontsize=10)
 plt.yticks(fontsize=10)
 plt.ylabel("Fit Residuals (lbs)", fontsize=14)
-plt.xlim(min(DateInts)-1.5,max(DateInts)+0.5)
+plt.xlim(min(DateIntsW)-2.5,max(DateIntsW)+2.5)
 plt.gca().yaxis.set_ticks_position("right")
 plt.gca().yaxis.set_label_position("right")
 
-plt.subplots_adjust(left=0.09, bottom=0.065, right=0.935, top=0.975, wspace=0, hspace=0)
+plt.subplots_adjust(left=0.095, bottom=0.065, right=0.935, top=0.975, wspace=0, hspace=0)
 plt.savefig('OutputImage.png',dpi=200)
 plt.show()
